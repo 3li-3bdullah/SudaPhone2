3b4243483f16275cc026d02d85c116ebb58f5d9a
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-28 23:51:26 +0200

    Added getxcontroller functions

--- sudaphone/lib/core/view_model/auth_view_model.dart
+++ sudaphone/lib/core/view_model/auth_view_model.dart
@@ -2,5 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthViewModel extends GetxController{
+  @override
+  void onInit() {
+    super.onInit();
+  }
+  @override
+  void onReady() {
+    super.onReady();
+  }
+  @override
+  void onClose() {
+    super.onClose();
+  }
   
 }
