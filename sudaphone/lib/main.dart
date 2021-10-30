c23c398f80999c449a2a8b3bb70d35eb9f090947
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-30 23:43:57 +0200

    Initialized the class main

--- sudaphone/lib/main.dart
+++ sudaphone/lib/main.dart
@@ -6,10 +6,12 @@ import '../view/auth/login.dart';
 void main()  {
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
