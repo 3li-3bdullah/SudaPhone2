cf4c98f5b34e62ea9ddd2223709d933b8e9abddb
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-01 23:25:22 +0200

    Added const word

--- sudaphone/lib/view/mobiledetails.dart
+++ sudaphone/lib/view/mobiledetails.dart
@@ -132,58 +132,58 @@ class _MobileDetailsState extends State<MobileDetails> {
             ),const SizedBox(height:2),
             //Start Column Specfiction
             Container(
-                padding: EdgeInsets.all(10),
+                padding:const EdgeInsets.all(10),
                 child: Column(
-                  children: [
+                  children: const [
 
                      CustomTextOfDetails(
                       text1:  "التقريب : ",
                       text2: "يقرب حتي 100 مرة أثناء التصوير بالكاميرا الزوم",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "دعم التصوير : ",
                       text2: "يدعم تصوير الفيديوهات بجودة ال 8K",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "الذاكرة العشوائية : ",
                       text2: "كبيرة جدا تعطيك مرونه في فتح الكثير من التطبيقات ولكن فعليا هل سنحتاج إلي كل هذه المساحة بالاخص مع قدومها من نوع LPDDR5",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "الكاميرا  : ",
                       text2:  "كامبرا قوية جدا قادرة علي المنافسة وكاد تكون الأفضل في 2020 ... مع كاميرا سيلفي مميزة 40 ميجا بيكسل",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "الشاشة  : ",
                       text2:  "ممتازة وكبيرة و بدقة عالية ومعدل تحديث 120 هرتز",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:   "البطارية : ",
                       text2: "كبيرة جدا والتي يتم توفيرها لأول مرة يهذه السعة في سلسلة ال S",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:   "الشحن : ",
                       text2: "يدعم الشحن السريع بقوة 45 واط حيث ستتمكن من شحن هاتفك من 0% إلي 100% خلال 74 دقيقة بالكامل",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:   "أداء المعالج : ",
                       text2: "قوي و يمتاز بأنه قوي من حيث الأداء",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ), SizedBox(height:2),
                   ],
                 )),
             //End second container
             //Start three container
             Container(
               color: Colors.white,
-              padding: EdgeInsets.all(10),
-              child: Center(
+              padding: const EdgeInsets.all(10),
+              child: const Center(
                 child: CustomText(
                   textAlign:TextAlign.center,
                   text: "العيوب",
@@ -196,47 +196,47 @@ class _MobileDetailsState extends State<MobileDetails> {
             Container(
                 padding: EdgeInsets.all(10),
                 child: Column(
-                  children: [
+                  children: const [
                     CustomTextOfDetails(
                       text1:  "السعر : ",
                       text2: "مرتفع وحجمة ثقيل",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "المنافذ : ",
                       text2: "لا يوجد منفذ 3.5 ملم",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "لمبة إشعارات : ",
                       text2: "لا توجد به لمبه إشعارات",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "الشبكة : ",
                       text2: "لا توجد به لمبه إشعارات",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "الكاميرا : ",
                       text2:  "بروز كبير في الشكل",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                    CustomTextOfDetails(
                       text1: "الألوان : ",
                       text2:  "محددة",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "الشرايح : ",
                       text2:   "لا يمكن استخدام شريحتين و كارت ميموري في نفس الوقت",
                       containerColor: kcontainerColor1,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:"الشاشة : ",
                       text2:   "كبيرة جدا في اليد وستحتاج إلي بعض الوقت للاعتياد عليها",
                       containerColor: kcontainerColor2,
-                    ),const SizedBox(height:2),
+                    ),SizedBox(height:2),
                   ],
                 )),
 
