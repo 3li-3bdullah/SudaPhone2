fb0effa9229d8eacadadc48c0bda3d3690ef1606
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-01 23:12:31 +0200

    Some editing

--- sudaphone/lib/view/aboutapp.dart
+++ sudaphone/lib/view/aboutapp.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone/view/widgets/custom_text.dart';
 // import '../compount/mydrawer.dart';
 
 class AboutApp extends StatefulWidget {
@@ -9,67 +11,71 @@ class AboutApp extends StatefulWidget {
 class _AboutAppState extends State<AboutApp> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Directionality(
-          textDirection: TextDirection.rtl,
-          child: Scaffold(
-              appBar: AppBar(
-                title: Text('حول التطبيق'),
-                centerTitle: true,
-              ),
-              ///drawer: MyDrawer(),
-              body: ListView(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.all(10),
-                    child: RichText(
-                        text: TextSpan(
-                      style: TextStyle(fontSize: 15),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text:
-                                "بعرض لك هذا التطبيق معظم انواع التلفوات وسعرها بالجنية السوداني",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                color: Colors.black)),
-                        TextSpan(
-                            text:
-                                "إذا واجهتك أي مشكلة يمكنك مراسلتي عبر البريد التالي : ",
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w700,
-                                color: Colors.black)),
-                        TextSpan(
-                            text: "Alieko.A50@gmail.com",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                color: Colors.green)),
-                      ],
-                    )),
-                  ),
-                  Padding(padding: EdgeInsets.all(50)),
-                
+    return Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+            appBar: AppBar(
+              title: const CustomText(
+                  text: "حول التطبيق",
+                  textAlign: TextAlign.center,
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.normal),
+              centerTitle: true,
+            ),
 
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 30),
-                    child: TextButton(
-                      onPressed: () {
-                        showModalBottomSheet(
-                            context: context,
-                            builder: (context) {
-                              return Container(color: Colors.green,
-                                  child: Text("Ali Abdullah Mqbool ^_^",
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(fontSize: 20)),
-                                  height: 100);
-                            });
-                      },
-                      child: Text("Programmed by :"),
-                    ),
-                  )
-                ],
-              ))),
-    );
+            ///drawer: MyDrawer(),
+            body: ListView(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.all(10),
+                  child: RichText(
+                      text: const TextSpan(
+                    style: TextStyle(fontSize: 15),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text:
+                              "بعرض لك هذا التطبيق معظم انواع التلفوات وسعرها بالجنية السوداني",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black)),
+                      TextSpan(
+                          text:
+                              "إذا واجهتك أي مشكلة يمكنك مراسلتي عبر البريد التالي : ",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black)),
+                      TextSpan(
+                          text: "Alieko.A50@gmail.com",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.green)),
+                    ],
+                  )),
+                ),
+                const Padding(padding: EdgeInsets.all(50)),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextButton(
+                    onPressed: () {
+                      Get.snackbar("Ali Abdullah",
+                          "Programmer he wanted to work just freelancer :) ");
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(colors: [Colors.green ,
+                         Colors.red]),
+                      ),
+                      child: const CustomText(text: "حول مطور التطبيق",
+                       textAlign: TextAlign.center, color: Colors.white,
+                        fontSize: 20, fontWeight: FontWeight.w600),
+                    )
+                  ),
+                )
+              ],
+            )));
   }
 }
