8bde8b32079e07b2ed93a2604a3ff49fa5fae583
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-05 00:25:44 +0200

    Initialized the class

---
+++ sudaphone/lib/view_model/auth_build_avatar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BuildAvatar extends StatelessWidget {
+  const BuildAvatar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
