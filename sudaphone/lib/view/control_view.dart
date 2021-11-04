dd9ac103492ba3a29366412a976a7794ec0708e0
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-04 21:51:58 +0200

    Created new file and initialize it

---
+++ sudaphone/lib/view/control_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone/core/view_model/auth_view_model.dart';
+
+class ControlView extends GetWidget<AuthViewModel>{
+  const ControlView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
