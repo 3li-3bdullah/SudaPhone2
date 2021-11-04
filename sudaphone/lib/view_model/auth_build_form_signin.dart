a7562bce6141c8899344f963c70c2455a2ea7eb6
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-05 00:03:23 +0200

    Initialized the class

---
+++ sudaphone/lib/view_model/auth_build_form_signin.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BuildFormSignIn extends StatelessWidget {
+  const BuildFormSignIn({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
