6ce9393bc14b143c0f79ae2140bb04aef5a001e3
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-04 21:58:26 +0200

    Added obx

--- sudaphone/lib/view/control_view.dart
+++ sudaphone/lib/view/control_view.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone/core/view_model/auth_view_model.dart';
+import 'package:sudaphone/view/auth/login.dart';
+import 'package:sudaphone/view/mydrawer.dart';
 
 class ControlView extends GetWidget<AuthViewModel>{
   const ControlView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Obx((){
+      return (Get.find<AuthViewModel>().user != null) ? MyDrawer() : LogIn();
+    });
   }
 
 }
