e0b924528296a161ba6685c3bb26eaffee463587
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-05 00:32:40 +0200

    I've made the fuction to class

--- sudaphone/lib/view_model/auth_build_avatar.dart
+++ sudaphone/lib/view_model/auth_build_avatar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sudaphone/view/widgets/constants.dart';
 import 'package:sudaphone/view_model/auth_view_model.dart';
 
 class BuildAvatar extends GetWidget<AuthViewModel> {
@@ -7,8 +8,43 @@ class BuildAvatar extends GetWidget<AuthViewModel> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+          color: controller.showsignin ? Colors.blue : kprimaryColor,
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: const [
+            BoxShadow(color: Colors.black, blurRadius: 3, spreadRadius: 0.1)
+          ]),
+      child: InkWell(
+        onTap: () {
+          controller.showsignin = !controller.showsignin;
+        },
+        child: Stack(
+          children: const [
+            Positioned(
+              top: 25,
+              right: 25,
+              child: Icon(
+                Icons.person_outline,
+                size: 50,
+                color: Colors.white,
+              ),
+            ),
+            Positioned(
+              top: 35,
+              left: 60,
+              child: Icon(
+                Icons.arrow_back,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
