e2417e0415a28177448c9ec1e981d910b10bfaad
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-06 00:06:47 +0200

    Removed the widgets color

--- sudaphone/lib/view/mobiledetails.dart
+++ sudaphone/lib/view/mobiledetails.dart
@@ -27,7 +27,7 @@ class _MobileDetailsState extends State<MobileDetails> {
             centerTitle: true,
           ),
           body: ListView(children: [
-            Container(
+            SizedBox(
                 height: 300,
                 child: GridTile(
                     child: Image.asset("example/images/product/samsung.jpg"),
@@ -84,37 +84,30 @@ class _MobileDetailsState extends State<MobileDetails> {
                     CustomTextOfDetails(
                       text1: "الموديل : ",
                       text2: "S20 Ultra",
-                      containerColor: kcontainerColor1,
                     ),  SizedBox(height:2),
                      CustomTextOfDetails(
                       text1:  "الشاشة : ",
                       text2:  "Super Amoled 5.5",
-                      containerColor: kcontainerColor2,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "نظام التشغيل : ",
                       text2:  "Android 10, One UI 2",
-                      containerColor: kcontainerColor1,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "الحماية  : ",
                       text2:   " IP68 مقاوم للماء , Gorilla Glass 6",
-                      containerColor: kcontainerColor2,
                     ), SizedBox(height:2),
                    CustomTextOfDetails(
                       text1:  "دقة الشاشة : ",
                       text2:   "6.9 بوصة بدقة QHD + بها ثقب صغير",
-                      containerColor: kcontainerColor1,
                     ), SizedBox(height:2),
                    CustomTextOfDetails(
                       text1:  "المعالج : ",
                       text2:   "Snapdragon 865, Eiyons 990",
-                      containerColor: kcontainerColor2,
                     ), SizedBox(height:2),
                   CustomTextOfDetails(
                       text1: "البطارية : ",
                       text2: "5000 MAH " + "" + "شحن سريع w15",
-                      containerColor: kcontainerColor1,
                     ), SizedBox(height:2),
                     
                   ],
@@ -123,11 +116,11 @@ class _MobileDetailsState extends State<MobileDetails> {
             Container(
               color: Colors.white,
               padding: const EdgeInsets.all(10),
-              child: const Center(
+              child:  Center(
                 child: CustomText(
                   textAlign: TextAlign.center,
                   text:  "المميزات",
-                  color: Colors.green,
+                  color: Colors.green.shade700,
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ) ,),
@@ -141,42 +134,34 @@ class _MobileDetailsState extends State<MobileDetails> {
                      CustomTextOfDetails(
                       text1:  "التقريب : ",
                       text2: "يقرب حتي 100 مرة أثناء التصوير بالكاميرا الزوم",
-                      containerColor: kcontainerColor1,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "دعم التصوير : ",
                       text2: "يدعم تصوير الفيديوهات بجودة ال 8K",
-                      containerColor: kcontainerColor2,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "الذاكرة العشوائية : ",
                       text2: "كبيرة جدا تعطيك مرونه في فتح الكثير من التطبيقات ولكن فعليا هل سنحتاج إلي كل هذه المساحة بالاخص مع قدومها من نوع LPDDR5",
-                      containerColor: kcontainerColor1,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "الكاميرا  : ",
                       text2:  "كامبرا قوية جدا قادرة علي المنافسة وكاد تكون الأفضل في 2020 ... مع كاميرا سيلفي مميزة 40 ميجا بيكسل",
-                      containerColor: kcontainerColor2,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:  "الشاشة  : ",
                       text2:  "ممتازة وكبيرة و بدقة عالية ومعدل تحديث 120 هرتز",
-                      containerColor: kcontainerColor1,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:   "البطارية : ",
                       text2: "كبيرة جدا والتي يتم توفيرها لأول مرة يهذه السعة في سلسلة ال S",
-                      containerColor: kcontainerColor2,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:   "الشحن : ",
                       text2: "يدعم الشحن السريع بقوة 45 واط حيث ستتمكن من شحن هاتفك من 0% إلي 100% خلال 74 دقيقة بالكامل",
-                      containerColor: kcontainerColor1,
                     ), SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:   "أداء المعالج : ",
                       text2: "قوي و يمتاز بأنه قوي من حيث الأداء",
-                      containerColor: kcontainerColor2,
                     ), SizedBox(height:2),
                   ],
                 )),
@@ -185,11 +170,11 @@ class _MobileDetailsState extends State<MobileDetails> {
             Container(
               color: Colors.white,
               padding: const EdgeInsets.all(10),
-              child: const Center(
+              child:  Center(
                 child: CustomText(
                   textAlign:TextAlign.center,
                   text: "العيوب",
-                  color: Colors.red,
+                  color: Colors.orange.shade800,
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ) ),
@@ -202,42 +187,34 @@ class _MobileDetailsState extends State<MobileDetails> {
                     CustomTextOfDetails(
                       text1:  "السعر : ",
                       text2: "مرتفع وحجمة ثقيل",
-                      containerColor: kcontainerColor1,
                     ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "المنافذ : ",
                       text2: "لا يوجد منفذ 3.5 ملم",
-                      containerColor: kcontainerColor2,
                     ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "لمبة إشعارات : ",
                       text2: "لا توجد به لمبه إشعارات",
-                      containerColor: kcontainerColor1,
                     ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "الشبكة : ",
                       text2: "لا توجد به لمبه إشعارات",
-                      containerColor: kcontainerColor2,
                     ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "الكاميرا : ",
                       text2:  "بروز كبير في الشكل",
-                      containerColor: kcontainerColor1,
                     ),SizedBox(height:2),
                    CustomTextOfDetails(
                       text1: "الألوان : ",
                       text2:  "محددة",
-                      containerColor: kcontainerColor2,
                     ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1: "الشرايح : ",
                       text2:   "لا يمكن استخدام شريحتين و كارت ميموري في نفس الوقت",
-                      containerColor: kcontainerColor1,
                     ),SizedBox(height:2),
                     CustomTextOfDetails(
                       text1:"الشاشة : ",
                       text2:   "كبيرة جدا في اليد وستحتاج إلي بعض الوقت للاعتياد عليها",
-                      containerColor: kcontainerColor2,
                     ),SizedBox(height:2),
                   ],
                 )),
