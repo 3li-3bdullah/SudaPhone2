4d837913f0d55bfe2569d853d2993072922da730
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-06 23:45:20 +0200

    Added listTile

--- sudaphone/lib/view/settings.dart
+++ sudaphone/lib/view/settings.dart
@@ -7,13 +7,23 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const CustomText(
-              text: 'الإعدادات',
-              textAlign: TextAlign.center,
-              color: Colors.blue,
-              fontSize: 25,
-              fontWeight: FontWeight.normal)),
-    );
+        appBar: AppBar(
+            title: const CustomText(
+                text: 'الإعدادات',
+                textAlign: TextAlign.center,
+                color: Colors.blue,
+                fontSize: 25,
+                fontWeight: FontWeight.normal)),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Expanded(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                  ListTile(
+                      title: Text("الوضع الليلي"),
+                      trailing: Icon(Icons.wb_sunny),
+                      onTap: () {})
+                ]))));
   }
 }
