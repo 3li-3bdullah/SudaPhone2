aa586d055d6f05b59ca738919eb03aed55592d98
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-06 23:31:31 +0200

    Added new custom widgets

--- sudaphone/lib/view/widgets/build_listtile.dart
+++ sudaphone/lib/view/widgets/build_listtile.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 class BuildListTile extends StatelessWidget {
-  const BuildListTile({ Key? key }) : super(key: key);
-
+   BuildListTile(
+      {
+     required this.text,
+     required this.onTap,
+     required this.icon,
+     required this.textColor,
+     required this.iconColor});
+  final String text;
+  final Function onTap;
+  final IconData icon;
+   Color textColor = Colors.white;
+   Color iconColor = Colors.white;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return InkWell(
+      child: ListTile(
+        title: Text(text,
+            style:  TextStyle(color: textColor, fontSize: 18)),
+        leading: Icon(icon, color: iconColor, size: 25),
+      ),
+      onTap: () {
+        onTap();
+      },
     );
   }
 }
