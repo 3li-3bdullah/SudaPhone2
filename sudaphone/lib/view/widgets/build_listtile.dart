a9bfcd52a3039b48daa932ad07a51f6b793b4c79
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-06 23:06:37 +0200

    Created new class widgets

---
+++ sudaphone/lib/view/widgets/build_listtile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BuildListTile extends StatelessWidget {
+  const BuildListTile({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
