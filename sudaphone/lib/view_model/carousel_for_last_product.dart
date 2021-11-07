c1eea65239aeceeb17438ff826f040016f0ddd83
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-07 23:19:26 +0200

    Renamed the last class

---
+++ sudaphone/lib/view_model/carousel_for_last_product.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone/view_model/auth_view_model.dart';
+
+class CategoriesCarousel extends GetWidget<AuthViewModel> {
+  const CategoriesCarousel({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child:Center()
+    );
+  }
+}
