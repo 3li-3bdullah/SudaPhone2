21b7cddd5dd15aebfdef110441d8975b0286c4e9
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-09 01:04:48 +0200

    Changed the positioned color

--- sudaphone/lib/view/widgets/build_positioned_bottom.dart
+++ sudaphone/lib/view/widgets/build_positioned_bottom.dart
@@ -13,16 +13,17 @@ class BuildPositionedBottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color bottomColor = const Color(0xfffbc2eb);
     return Positioned(
       //top : 300
-      top: mdw * 1.2, // this will go down
+      top: mdw * 1.1, // this will go down
       right: mdw / 1.5,
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 500),
         height: mdw,
         width: mdw,
         decoration: BoxDecoration(borderRadius: BorderRadius.circular(mdw),
-         color: showsignin ? Colors.blue.withOpacity(.5) : kprimaryColor.withOpacity(.5)),
+         color: showsignin ? Colors.blue.withOpacity(.5) : bottomColor),
       ),
     );
   }
