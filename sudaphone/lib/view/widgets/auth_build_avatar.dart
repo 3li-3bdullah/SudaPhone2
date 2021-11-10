a137ca7b9064b2e766b4c079a87e20258a1b11aa
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-10 16:44:28 +0200

    Transformed the class to widgets folder

---
+++ sudaphone/lib/view/widgets/auth_build_avatar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone/view/widgets/constants.dart';
+import 'package:sudaphone/view_model/auth_view_model.dart';
+
+class BuildAvatar extends GetWidget<AuthViewModel> {
+  const BuildAvatar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color RegAC = const Color(0xffcb98ed);
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+          color: controller.showsignin ? Colors.grey.shade300 : RegAC,
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: const [
+            BoxShadow(color: Colors.black, blurRadius: 3, spreadRadius: 0.1)
+          ]),
+      child: InkWell(
+        onTap: () {
+          controller.showsignin = !controller.showsignin;
+        },
+        child: Stack(
+          children: const [
+            Positioned(
+              top: 25,
+              right: 25,
+              child: Icon(
+                Icons.person_outline,
+                size: 50,
+                color: Colors.white,
+              ),
+            ),
+            Positioned(
+              top: 35,
+              left: 60,
+              child: Icon(
+                Icons.arrow_back,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
