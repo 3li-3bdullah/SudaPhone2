98b2177bac0aa84b04164f2cf75fac40fff35c77
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-12 23:57:54 +0200

    Added listTile and some widgets

--- sudaphone/lib/view/settings.dart
+++ sudaphone/lib/view/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sudaphone/view/widgets/custom_text.dart';
 
 class Settings extends StatelessWidget {
@@ -7,30 +8,41 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Directionality(
-    textDirection:TextDirection.ltr,
+      textDirection: TextDirection.ltr,
       child: Scaffold(
           appBar: AppBar(
-              title: const CustomText(
-                  text: 'الإعدادات',
-                  textAlign: TextAlign.center,
-                  color: Colors.black,
-                  fontSize: 25,
-                  fontWeight: FontWeight.normal),
-                  centerTitle: true,
-                  backgroundColor: Colors.white,
-                  ),
+            title: const CustomText(
+                text: 'الإعدادات',
+                textAlign: TextAlign.center,
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.normal),
+            centerTitle: true,
+            backgroundColor: Colors.white,
+          ),
           body: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children:  [
-                ListTile(
-                    title: const Text("الوضع فاتح / داكن"),
-                    onTap: (){
-                      
-                    },
+                  children: [
+                    ListTile(
+                      title: const Text("الوضع فاتح / داكن"),
+                      onTap: () {
+                        Get.bottomSheet(
+                          Container(
+                            child: Wrap(
+                              children: [
+                                ListTile(
+                                  title:Text( "الوضع الفاتح"),
+                                  leading: Icon(Icons.wb_sunny_outlined),
+                                 onTap: (){},),
+                              ],
+                            ),
+                          )
+                        );
+                      },
                     )
-              ]))),
+                  ]))),
     );
   }
 }
