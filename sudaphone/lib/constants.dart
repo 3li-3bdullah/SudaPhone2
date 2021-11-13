8c8cc91105e9be7a3b3fae1d9f3f9ddecb8ddceb
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-13 23:47:57 +0200

    Added extra const color

--- sudaphone/lib/constants.dart
+++ sudaphone/lib/constants.dart
@@ -7,4 +7,6 @@ const kcontainerColor2 = Color(0xff808488);
 const kappDetails = Color(0xff555555);
 const kfillColor = Color(0xffe0e0e0);
 const kenabledColor = Color(0xffbdbdbd);
-const ksignColor1 =Color(0xff78a3eb);
+const ksignUpColor1 =Color(0xff78a3eb);
+ const ksignUpColor2 =Color(0xfffbc2eb);
+  const ksignInColor1 =Color(0xfff5d099);
