395f73d0281b5bda8718a70415388fcdc5f0249e
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-17 23:40:26 +0200

    Initialized the class

---
+++ sudaphone/lib/view/categories_pages/huawei.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Huawei extends StatelessWidget {
+  const Huawei({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
