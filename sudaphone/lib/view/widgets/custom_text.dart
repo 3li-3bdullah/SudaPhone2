6810558c6a23042bf6ae818b055dcf8a1b095529
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-17 18:58:26 +0200

    more edit to make the programmer can use anywidgets :)

--- sudaphone/lib/view/widgets/custom_text.dart
+++ sudaphone/lib/view/widgets/custom_text.dart
@@ -2,26 +2,26 @@ import 'package:flutter/material.dart';
 
 class CustomText extends StatelessWidget {
   const CustomText({
-    context,
-  required  this.text,
-  required  this.textAlign,
-  required  this.color,
-  required  this.fontSize,
-  required  this.fontWeight,
+    ///context,
+    this.text,
+    this.textAlign,
+    this.color,
+    this.fontSize,
+    this.fontWeight,
   });
-  final String text;
-  final TextAlign textAlign;
-  final Color color;
-  final double fontSize;
-  final FontWeight fontWeight;
+  final String? text;
+  final TextAlign? textAlign;
+  final Color? color;
+  final double? fontSize;
+  final FontWeight? fontWeight;
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      text,
-      textAlign: textAlign,
+      text!,
+      textAlign: textAlign!,
       style:
-          TextStyle(color: color, fontSize: fontSize, fontWeight: fontWeight),
+          TextStyle(color: color!, fontSize: fontSize!, fontWeight: fontWeight!),
     );
   }
 }
