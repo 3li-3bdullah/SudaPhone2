c0531c9a9aece14368deb82f10d08e036a4deefb
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-20 19:14:10 +0200

    uncomments the firebase initialized

--- sudaphone/lib/main.dart
+++ sudaphone/lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone/view/control_view.dart';
 
 
-void main()  {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
