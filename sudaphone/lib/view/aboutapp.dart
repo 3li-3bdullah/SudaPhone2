90841edd76807cecdc799bef429c31cc534b39cc
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-23 21:23:12 +0200

    Converted the class to statelesswidget

--- sudaphone/lib/view/aboutapp.dart
+++ sudaphone/lib/view/aboutapp.dart
@@ -3,14 +3,9 @@ import 'package:get/get.dart';
 import 'package:sudaphone/view/widgets/custom_text.dart';
 // import '../compount/mydrawer.dart';
 
-class AboutApp extends StatefulWidget {
+class AboutApp extends StatelessWidget {
   const AboutApp({Key? key}) : super(key: key);
 
-  @override
-  _AboutAppState createState() => _AboutAppState();
-}
-
-class _AboutAppState extends State<AboutApp> {
   @override
   Widget build(BuildContext context) {
     return Directionality(
