c5df5b627b2358a3b9a30eebf0f0a544cec2abf8
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-23 21:23:29 +0200

    some editing

--- sudaphone/lib/view/control_view.dart
+++ sudaphone/lib/view/control_view.dart
@@ -4,13 +4,13 @@ import 'package:sudaphone/view/login.dart';
 import 'package:sudaphone/view/mydrawer.dart';
 import 'package:sudaphone/view_model/login_view_model.dart';
 
-class ControlView extends GetWidget<AuthViewModel> {
+class ControlView extends GetWidget<LoginViewModel> {
   const ControlView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      return (Get.find<AuthViewModel>().user != null) ?  MyDrawer() : LogIn();
+      return (Get.find<LoginViewModel>().user != null) ?  const MyDrawer() : LogIn();
     });
   }
 }
