d698144425fb8a2187df144491ae8a4250d4571f
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-26 09:47:23 +0200

    Some added

--- sudaphone/lib/view/categories.dart
+++ sudaphone/lib/view/categories.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sudaphone/view/categories_pages/huawei.dart';
+import 'package:sudaphone/view/categories_pages/iphone.dart';
+import 'package:sudaphone/view/categories_pages/lenovo.dart';
+import 'package:sudaphone/view/categories_pages/nokia.dart';
+import 'package:sudaphone/view/categories_pages/oppo.dart';
+import 'package:sudaphone/view/categories_pages/realme.dart';
 import 'package:sudaphone/view/categories_pages/samsung.dart';
+import 'package:sudaphone/view/categories_pages/tecno.dart';
+import 'package:sudaphone/view/categories_pages/vivo.dart';
+import 'package:sudaphone/view/categories_pages/xiaomi.dart';
 import 'package:sudaphone/view/widgets/categories_card.dart';
 import 'package:sudaphone/view/widgets/custom_text.dart';
+
 class Categories extends StatelessWidget {
   const Categories({Key? key}) : super(key: key);
 
@@ -17,10 +27,15 @@ class Categories extends StatelessWidget {
             child: Scaffold(
               appBar: AppBar(
                 backgroundColor: Colors.white,
-                title: const CustomText(text: "الأقسام", textAlign: TextAlign.center, color: Colors.black,
-                 fontSize: 30, fontWeight: FontWeight.normal),
+                title: const CustomText(
+                    text: "الأقسام",
+                    textAlign: TextAlign.center,
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.normal),
                 centerTitle: true,
               ),
+
               /// drawer: MyDrawer(),
               body: Container(
                 height: double.infinity,
@@ -35,56 +50,73 @@ class Categories extends StatelessWidget {
                       imageCat: "assets/images/category/samsung.png",
                       text: "Samsung",
                       onTap: () {
-                        Get.to(Samsung()
-                        ,transition: Transition.zoom,
-                        curve: Curves.bounceInOut
-                        );
+                        Get.to(Samsung(),
+                            transition: Transition.zoom,
+                            curve: Curves.bounceInOut);
                       },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/huawei.png",
                       text: "Huawei",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Huawei());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/apple.jpg",
-                      text: "Apple",
-                      onTap: () {},
+                      text: "Iphone",
+                      onTap: () {
+                        Get.to(Iphone());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "example/images/category/oppo.png",
                       text: "Oppo",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Oppo());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/xiaomi.png",
                       text: "Xiaomi",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Xiaomi());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/lenovo.png",
                       text: "Lenovo",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Lenovo());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/realme.png",
                       text: "Realme",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Realme());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/tecno.png",
                       text: "Tecno",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Tecno());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/vivo.jpg",
                       text: "Vivo",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Vivo());
+                      },
                     ),
                     CategoriesCard(
                       imageCat: "assets/images/category/nokia.png",
                       text: "Nokia",
-                      onTap: () {},
+                      onTap: () {
+                        Get.to(Nokia());
+                      },
                     ),
                   ],
                 ),
