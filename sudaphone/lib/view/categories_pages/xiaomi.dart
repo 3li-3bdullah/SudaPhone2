725e9ce7efa49b82e54988c0b370a9dc853fb2d3
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-26 09:49:46 +0200

    Renamed the class

--- sudaphone/lib/view/categories_pages/xiaomi.dart
+++ sudaphone/lib/view/categories_pages/xiaomi.dart
@@ -3,12 +3,12 @@ import 'package:sudaphone/view/categories_pages/moblist.dart';
 import 'package:sudaphone/view/widgets/custom_text.dart';
 
 
-class Lenovo extends StatelessWidget {
-   Lenovo({ Key? key }) : super(key: key);
+class Xiaomi extends StatelessWidget {
+   Xiaomi({ Key? key }) : super(key: key);
 
   final List<Map<String, String>> _mobilelist = <Map<String, String>>[
     {
-      'image': 'example/images/product/xiaomi.jpg',
+      'image': 'assets/images/product/xiaomi.jpg',
       'name': 'Mate 40 Pro',
       'camera': '108 Megapixel',
       'cpu': 'Snapdragon 865 ثماني',
