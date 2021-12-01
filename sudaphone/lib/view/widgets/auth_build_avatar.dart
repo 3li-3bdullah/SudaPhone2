0ccc6afdb55c5693bc0f140b378c9f39c5c1e7b6
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-01 21:25:28 +0200

    Added the entire body in obx

--- sudaphone/lib/view/widgets/auth_build_avatar.dart
+++ sudaphone/lib/view/widgets/auth_build_avatar.dart
@@ -5,48 +5,49 @@ import 'package:sudaphone/view_model/login_view_model.dart';
 import '../../constants.dart';
 
 class BuildAvatar extends GetWidget<LoginViewModel> {
-  const BuildAvatar({ Key? key }) : super(key: key);
+  const BuildAvatar({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
-    Color regAC = const Color(0xffcb99ed);
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 500),
-      height: 100,
-      width: 100,
-      decoration: BoxDecoration(
-          color: controller.showsignin ? ksignInColor1 : ksignUpColor2,
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: const [
-            BoxShadow(color: Colors.black, blurRadius: 3, spreadRadius: 0.1)
-          ]),
-      child: InkWell(
-        onTap: () {
-          controller.showsignin = !controller.showsignin;
-        },
-        child: Stack(
-          children: const [
-            Positioned(
-              top: 25,
-              right: 25,
-              child: Icon(
-                Icons.person_outline,
-                size: 50,
-                color:  Colors.black,
-              ),
+    return Obx(() => AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+              color:
+                  controller.showsignin.value ? ksignInColor1 : ksignUpColor2,
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: const [
+                BoxShadow(color: Colors.black, blurRadius: 3, spreadRadius: 0.1)
+              ]),
+          child: InkWell(
+            onTap: () {
+              controller.showsignin(false);
+            },
+            child: Stack(
+              children: const [
+                Positioned(
+                  top: 25,
+                  right: 25,
+                  child: Icon(
+                    Icons.person_outline,
+                    size: 50,
+                    color: Colors.black,
+                  ),
+                ),
+                Positioned(
+                  top: 35,
+                  left: 60,
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
-            Positioned(
-              top: 35,
-              left: 60,
-              child: Icon(
-                Icons.arrow_back,
-                size: 30,
-                color:  Colors.black,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
