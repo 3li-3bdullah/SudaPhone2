b8306a4ed7496f250de9bc43c4720158ce384625
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-03 23:29:17 +0200

    Initialized firebase becasue the has come the use it :)

--- sudaphone/lib/main.dart
+++ sudaphone/lib/main.dart
@@ -1,12 +1,13 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone/view/control_view.dart';
+// import 'package:sudaphone/view/mydrawer.dart';
 
 
-void main()  {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
