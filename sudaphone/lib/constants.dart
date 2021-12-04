2b48ea75a3c6c2b31aaab7f8be8c0b89a35202e7
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-04 23:35:39 +0200

    Added new const color and size

--- sudaphone/lib/constants.dart
+++ sudaphone/lib/constants.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 const kfillColor = Color(0xffe0e0e0);
 const kenabledColor = Color(0xffdd145d);
 const ksignUpColor1 =Color(0xff78a3eb);
- const ksignUpColor2 =Color(0xfffbc2eb);
-  const ksignInColor1 =Color(0xfff5d099);
+const ksignUpColor2 =Color(0xfffbc2eb);
+const ksignInColor1 =Color(0xfff5d099);
+
+const kTextColor = Color(0xff3c4046);
+const kBackgroundColor = Color(0xfff9f8fd);
+
+const double kDefaultPadding = 20.0;
