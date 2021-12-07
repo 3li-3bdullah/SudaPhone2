a52a4c291b639a2fa642998b1dfb38e36e0e0e96
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-07 23:48:55 +0200

    Added new color

--- sudaphone/lib/constants.dart
+++ sudaphone/lib/constants.dart
@@ -5,8 +5,8 @@ const kenabledColor = Color(0xffdd145d);
 const ksignUpColor1 =Color(0xff78a3eb);
 const ksignUpColor2 =Color(0xfffbc2eb);
 const ksignInColor1 =Color(0xfff5d099);
-
 const kTextColor = Color(0xff3c4046);
 const kBackgroundColor = Color(0xfff9f8fd);
+const kPrimaryColor = Color(0xff0c9869);
 
 const double kDefaultPadding = 20.0;
