a32670e02ace51306056719c1b9e5ea432ddd752
Ali Abdullah <89943558+3li-3bdullah@users.noreply.github.com>
2021-12-07 19:45:41 +0200

    Delete sudaphone/lib/helper directory
    I deleted unnecessary directory

--- sudaphone/lib/helper/binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:sudaphone/view_model/login_view_model.dart';
-
-class Binding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => AuthViewModel());
-  }
-}
