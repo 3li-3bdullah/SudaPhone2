1471e8dedfc993daaa5f693d3ce88789555e2ca6
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-18 23:32:57 +0200

    Commit the class to use it later

--- sudaphone/lib/binding/binding.dart
+++ sudaphone/lib/binding/binding.dart
@@ -1,9 +1,9 @@
-import 'package:get/get.dart';
-import 'package:sudaphone/view_model/login_view_model.dart';
+// import 'package:get/get.dart';
+// import 'package:sudaphone/view_model/login_view_model.dart';
 
-class Binding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => LoginViewModel());
-  }
-}
+// class Binding extends Bindings {
+//   @override
+//   void dependencies() {
+//     Get.lazyPut(() => LoginViewModel());
+//   }
+// }
