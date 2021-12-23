04e0897b3e34137666e24ea66c222fb58f438823
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 16:01:24 +0200

    created varialbes and used it at mydrawer and some codes

--- sudaphone/lib/view/splash_screen.dart
+++ sudaphone/lib/view/splash_screen.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sudaphone/model/drawer_model.dart';
 import 'package:sudaphone/view/my_drawer.dart';
 import 'package:sudaphone/view/screen.dart';
 import 'package:sudaphone/view_model/splash_screen_view_model.dart';
 
 class SplashScreen extends GetWidget<SplashScreenViewModel> {
   final scontroller = Get.lazyPut(() => SplashScreenViewModel());
-   SplashScreen({Key? key}) : super(key: key);
+  SplashScreen({Key? key}) : super(key: key);
+  DrawerItem dItem = DrawerItems.home;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue.shade800,
       body: Stack(
+        children: [
         drawerHolder(),
         screenHolder(),
+        ],
       ),
     );
   }
 
   // Widget drawerHolder -----------
-  Widget drawerHolder() => SafeArea(
-          child: MyDrawer(
-        onSelectedDrawerItem: (e) {},
+  Widget drawerHolder() => SafeArea(child: MyDrawer(
+        onSelectedDrawerItem: (e) {
+          dItem = e;
+          return controller.closeDrawer();
+        },
       ));
   // Widget screenHolder -----------
   Widget screenHolder() {
-    return Obx(() =>  ClipRRect(
-          borderRadius: BorderRadius.circular(controller.isDrawerOpen!.value ? 20 : 0), child: Screen()),
+    return Obx(
+      () => AbsorbPointer(
+        absorbing: controller.isDrawerOpen!.value,
+        child: ClipRRect(
+            borderRadius:
+                BorderRadius.circular(controller.isDrawerOpen!.value ? 20 : 0),
+            child: Screen()),
+      ),
     );
   }
 }
