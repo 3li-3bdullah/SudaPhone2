12e4d8457d3b444bd8c68ba8743d7a30d7d9a0a0
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 15:39:32 +0200

    Created new file and 4 variables

---
+++ sudaphone/lib/view_model/splash_screen_view_model.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:get/get.dart';
+
+class SplashScreenViewModel extends GetxController{
+
+  
+  RxDouble? xOffset = 0.0.obs;
+  RxDouble? yOffset = 0.0.obs;
+  RxDouble? scaleFactor = 1.0.obs;
+  RxBool? isDrawerOpen = false.obs;
+
+  
+}
