00079b14a596a4c1a837fc098db7e26064cd7363
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 15:45:53 +0200

    Created widget and added inside constructor screen

--- sudaphone/lib/view/splash_screen.dart
+++ sudaphone/lib/view/splash_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sudaphone/view/my_drawer.dart';
+import 'package:sudaphone/view/screen.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({ Key? key }) : super(key: key);
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +11,18 @@ class SplashScreen extends StatelessWidget {
       backgroundColor: Colors.blue.shade800,
       body: Stack(
         drawerHolder(),
+        screenHolder(),
       ),
     );
   }
+
   // Widget drawerHolder -----------
-  Widget drawerHolder()=>SafeArea(child: MyDrawer(onSelectedDrawerItem: (e) { 
-
-   },
-       
+  Widget drawerHolder() => SafeArea(
+          child: MyDrawer(
+        onSelectedDrawerItem: (e) {},
       ));
-  
-
+  // Widget screenHolder -----------
+  Widget screenHolder() {
+    return Screen();
+  }
 }
