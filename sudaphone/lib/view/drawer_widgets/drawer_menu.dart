add54fb54ce1aad6cc746f7463eeb3e606eaa227
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 15:02:39 +0200

    Created variables and iconButton

--- sudaphone/lib/view/drawer_widgets/drawer_menu.dart
+++ sudaphone/lib/view/drawer_widgets/drawer_menu.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DrawerMenu extends StatelessWidget {
+  final VoidCallback? onClicked;   
+  const DrawerMenu({Key? key, this.onClicked}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return IconButton(
+      icon: const FaIcon(FontAwesomeIcons.alignLeft),
+      onPressed:onClicked);
   }
 }
