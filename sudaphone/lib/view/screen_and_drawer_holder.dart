5c86d264cf3f1f9f9a4a5c959761e40590f4b60c
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 15:24:37 +0200

    Renamed the file screen holder to created class screen and drawer holder

---
+++ sudaphone/lib/view/screen_and_drawer_holder.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScreenAndDrawerHolder extends StatelessWidget {
+  
+  const ScreenAndDrawerHolder({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
