23241a294ae54db72dac16f723723d0b3c5911cd
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 15:52:02 +0200

    Added obx & cliprrect and impelement the package get and splash viewmodel

--- sudaphone/lib/view/splash_screen.dart
+++ sudaphone/lib/view/splash_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sudaphone/view/my_drawer.dart';
 import 'package:sudaphone/view/screen.dart';
+import 'package:sudaphone/view_model/splash_screen_view_model.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+class SplashScreen extends GetWidget<SplashScreenViewModel> {
+  final scontroller = Get.lazyPut(() => SplashScreenViewModel());
+   SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +26,8 @@ class SplashScreen extends StatelessWidget {
       ));
   // Widget screenHolder -----------
   Widget screenHolder() {
-    return Screen();
+    return Obx(() =>  ClipRRect(
+          borderRadius: BorderRadius.circular(controller.isDrawerOpen!.value ? 20 : 0), child: Screen()),
+    );
   }
 }
