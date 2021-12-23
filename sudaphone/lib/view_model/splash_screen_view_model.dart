6b1dd30cd46b5863d207adb4e82ecd67d566f0ed
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 15:47:26 +0200

    Created 2 method to open and close the drawer

--- sudaphone/lib/view_model/splash_screen_view_model.dart
+++ sudaphone/lib/view_model/splash_screen_view_model.dart
@@ -10,5 +10,17 @@ class SplashScreenViewModel extends GetxController{
   RxDouble? scaleFactor = 1.0.obs;
   RxBool? isDrawerOpen = false.obs;
 
-  
+  openDrawer() {
+    xOffset!.value = 230.0;
+    yOffset!.value = 150.0;
+    scaleFactor!.value = 0.6;
+    isDrawerOpen!.value = true;
+  }
+
+  void closeDrawer() {
+    xOffset!.value = 0.0;
+    yOffset!.value = 0.0;
+    scaleFactor!.value = 1.0;
+    isDrawerOpen!.value = false;
+  }
 }
