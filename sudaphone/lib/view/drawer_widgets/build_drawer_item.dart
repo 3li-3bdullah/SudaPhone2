264b8f87cd9bc42b6f84b311ab83859abd1ac7cb
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-23 14:38:40 +0200

    Created new file and added stl widget

---
+++ sudaphone/lib/view/drawer_widgets/build_drawer_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BuildDrawerItem extends StatelessWidget {
+  const BuildDrawerItem({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
