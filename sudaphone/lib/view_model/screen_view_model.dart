2ea70c31ee442ab416b5f4c139a217977bb682e6
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-25 23:07:19 +0200

    created observable var and two method for change the value

--- sudaphone/lib/view_model/screen_view_model.dart
+++ sudaphone/lib/view_model/screen_view_model.dart
@@ -1,9 +1,14 @@
 import 'package:get/get.dart';
 
+class ScreenViewModel extends GetxController {
+  RxInt activeIndex = 0.obs;
+  RxDouble value = 0.0.obs;
 
-class ScreenViewModel extends GetxController{
+ void valueOne() {
+    value(1.0);
+  }
 
- RxInt activeIndex = 0.obs;
- 
-
+  void valueZero() {
+    value(0.0);
+  }
 }
