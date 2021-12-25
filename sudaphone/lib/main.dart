a71f91ee22492456edc20af440a607302cb2b12a
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-25 23:05:47 +0200

    made the class main start with mydrawer class

--- sudaphone/lib/main.dart
+++ sudaphone/lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sudaphone/view/control_view.dart';
-// import 'package:sudaphone/view/mydrawer.dart';
+import 'package:sudaphone/view/mydrawer.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+   MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "SudaPhone",
-     theme: ThemeData.light(),
-      home: const ControlView(),
+      theme: ThemeData.light(),
+      home: const MyDrawer(),
     );
   }
 }
