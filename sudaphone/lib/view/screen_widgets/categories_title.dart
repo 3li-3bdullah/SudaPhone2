547e386840276c5a1adcf2998b8fd524e5ac739d
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-26 23:37:20 +0200

    Created class and added inside row and 2 text

---
+++ sudaphone/lib/view/screen_widgets/categories_title.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CategoriesTitle extends StatelessWidget {
+  CategoriesTitle({Key? key , this.text,this.press}) : super(key: key);
+  String? text;
+  Function? press;
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text(
+        text!,
+        style: const TextStyle(fontSize: 20),
+      ),
+      const Spacer(),
+      Material(
+        color: Colors.green,
+        child: MaterialButton(
+            onPressed: () {
+              press!();
+            },
+            child: const Text("more", style: TextStyle(color: Colors.white))),
+      )
+    ]);
+  }
+}
