53ec14116d608d023fd3420f3ea678f650922b99
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-26 21:30:50 +0200

    Removed the custom text and added text widget

--- sudaphone/lib/view/settings.dart
+++ sudaphone/lib/view/settings.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sudaphone/view/widgets/custom_text.dart';
 
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
@@ -11,48 +10,36 @@ class Settings extends StatelessWidget {
       textDirection: TextDirection.rtl,
       child: Scaffold(
           appBar: AppBar(
-            title: const CustomText(
-                text: 'الإعدادات',
-                textAlign: TextAlign.center,
-                fontSize: 25,
-                fontWeight: FontWeight.normal),
+            title: const Text(
+                 'الإعدادات',),
             centerTitle: true,
           ),
           body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          title: const Text("الوضع فاتح / داكن"),
-                          trailing: const Icon(Icons.wb_sunny),
-                          onTap: () {
-                            Get.bottomSheet(Wrap(
-                              children: [
-                                ListTile(
-                                  title: const Text("الوضع الفاتح"),
-                                  leading: const Icon(Icons.wb_sunny_outlined),
-                                  onTap: () {
-                                    Get.changeTheme(ThemeData.light());
-                                  },
-                                ),
-                                ListTile(
-                                  title: const Text("الوضع الداكن"),
-                                  leading: const Icon(Icons.wb_sunny),
-                                  onTap: () {
-                                    Get.changeTheme(ThemeData.dark());
-                                  },
-                                ),
-                              ],
-                            ));
-                          },
-                        ),
-                      ],
-                    )
-                  ]))),
+              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+              child: ListTile(
+                title: const Text("الوضع فاتح / داكن"),
+                trailing: const Icon(Icons.wb_sunny),
+                onTap: () {
+                  Get.bottomSheet(Wrap(
+                    children: [
+                      ListTile(
+                        title: const Text("الوضع الفاتح"),
+                        leading: const Icon(Icons.wb_sunny_outlined),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.light());
+                        },
+                      ),
+                      ListTile(
+                        title: const Text("الوضع الداكن"),
+                        leading: const Icon(Icons.wb_sunny),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.dark());
+                        },
+                      ),
+                    ],
+                  ));
+                },
+              ))),
     );
   }
 }
