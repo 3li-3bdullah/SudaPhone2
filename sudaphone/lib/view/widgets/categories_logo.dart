6a838abafe952ebd070783d7a457d54f3cea8890
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-26 21:34:02 +0200

    Wrapped image.asset with container and added some features , changed the width and height

--- sudaphone/lib/view/widgets/categories_logo.dart
+++ sudaphone/lib/view/widgets/categories_logo.dart
@@ -13,13 +13,18 @@ class CategoriesLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 100,
-        width: 100,
+        height: 80,
+        width: 80,
         child: ListTile(
             onTap: () {
               onTap();
             },
-            title: Image.asset(imageLogo, fit: BoxFit.cover),
+            title: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      image: AssetImage(imageLogo), fit: BoxFit.cover)),
+            ),
             subtitle: CustomText(
               text: text,
               textAlign: TextAlign.center,
