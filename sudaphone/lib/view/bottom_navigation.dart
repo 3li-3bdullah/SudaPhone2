89968669563e1342db799840100a2550d97f1b9f
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-27 22:02:45 +0200

    Added list of widgets and added four screens

--- sudaphone/lib/view/bottom_navigation.dart
+++ sudaphone/lib/view/bottom_navigation.dart
@@ -1,40 +1,49 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sudaphone/view/categories.dart';
+import 'package:sudaphone/view/mydrawer.dart';
+import 'package:sudaphone/view/post.dart';
+import 'package:sudaphone/view/settings.dart';
 import 'package:sudaphone/view_model/screen_view_model.dart';
 
 class BottomNavigation extends GetWidget<ScreenViewModel> {
-  const BottomNavigation({ Key? key }) : super(key: key);
-
+  BottomNavigation({Key? key}) : super(key: key);
+  final List<Widget> screens = [
+    const MyDrawer(),
+    const Categories(),
+    const Post(),
+    const Settings(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Obx(()=>
-               BottomNavyBar(
-                  selectedIndex: controller.activeIndex.value,
-                  showElevation: true,
-                  onItemSelected: (index) {
-                   controller.activeIndex.value = index;
-                  },
-                  items: [
-                    BottomNavyBarItem(
-                        icon: const Icon(Icons.apps),
-                        title: const Text("Home"),
-                        activeColor: Colors.red),
-                    BottomNavyBarItem(
-                        icon: const Icon(Icons.category),
-                        title: const Text("Categories"),
-                        activeColor: Colors.purpleAccent),
-                    BottomNavyBarItem(
-                        icon: const Icon(Icons.post_add),
-                        title: const Text("Posts"),
-                        activeColor: Colors.pink),
-                    BottomNavyBarItem(
-                        icon: const Icon(Icons.settings),
-                        title: const Text("Settings"),
-                        activeColor: Colors.blue),
-                  ]),
-            ),
+      bottomNavigationBar: Obx(
+        () => BottomNavyBar(
+            selectedIndex: controller.activeIndex.value,
+            showElevation: true,
+            onItemSelected: (index) {
+              controller.activeIndex.value = index;
+            },
+            items: [
+              BottomNavyBarItem(
+                  icon: const Icon(Icons.apps),
+                  title: const Text("Home"),
+                  activeColor: Colors.red),
+              BottomNavyBarItem(
+                  icon: const Icon(Icons.category),
+                  title: const Text("Categories"),
+                  activeColor: Colors.purpleAccent),
+              BottomNavyBarItem(
+                  icon: const Icon(Icons.post_add),
+                  title: const Text("Posts"),
+                  activeColor: Colors.pink),
+              BottomNavyBarItem(
+                  icon: const Icon(Icons.settings),
+                  title: const Text("Settings"),
+                  activeColor: Colors.blue),
+            ]),
+      ),
     );
   }
 }
