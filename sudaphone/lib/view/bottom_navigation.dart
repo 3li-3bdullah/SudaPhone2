459ccc0f81b7fa492a6392e273955d90b723d85b
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-27 21:56:40 +0200

    Created new file and initialized the class

---
+++ sudaphone/lib/view/bottom_navigation.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
