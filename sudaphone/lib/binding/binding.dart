07b1ff6ba8faa52c60cb5d6320657d67031546d0
3li-3bdullah <PROGRMMER7@gmail.com>
2022-01-11 23:43:25 +0200

    changes that i uploaded at main branch

--- sudaphone/lib/binding/binding.dart
+++ sudaphone/lib/binding/binding.dart
@@ -1,9 +1,11 @@
-// import 'package:get/get.dart';
-// import 'package:sudaphone/view_model/login_view_model.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone/view_model/login_view_model.dart';
+import 'package:sudaphone/view_model/screen_view_model.dart';
 
-// class Binding extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut(() => LoginViewModel());
-//   }
-// }
+class Binding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => LoginViewModel() , fenix: true);
+    Get.lazyPut(() => ScreenViewModel() , fenix: true);
+  }
+}
