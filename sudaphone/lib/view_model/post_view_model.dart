585a3466f1f9f9642bd687d000d23dbb3da319e4
3li-3bdullah <PROGRMMER7@gmail.com>
2022-01-28 21:34:27 +0200

    declared one variable and created method

--- sudaphone/lib/view_model/post_view_model.dart
+++ sudaphone/lib/view_model/post_view_model.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
-class PostViewModel extends GetxController{
-  
+class PostViewModel extends GetxController {
+  final postKey = GlobalKey<FormState>();
+  addPost() {
+    var formState = postKey.currentState;
+    if(formState!.validate()){
+      
+    }
+  }
 }
